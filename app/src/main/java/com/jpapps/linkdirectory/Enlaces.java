b98cf1eb6f769dd8ecf57100f15892aa5504d5aa
jprecreativo <jprecreativo@gmail.com>
2019-06-09 17:40:09 +0200

    Now links can be added.

--- app/src/main/java/com/jpapps/linkdirectory/Enlaces.java
+++ app/src/main/java/com/jpapps/linkdirectory/Enlaces.java
@@ -1,19 +1,23 @@
 package com.jpapps.linkdirectory;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Enlaces extends AppCompatActivity {
 
-    private static final ArrayList<String> arrayEnlaces = new ArrayList(Arrays.asList("http://www.google.es"));
+    private static final ArrayList<String> arrayEnlaces = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +39,42 @@ public class Enlaces extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void añadirEnlace_Click(View view)
+    {
+        final EditText editText = new EditText(this);
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Añadir enlace");
+        alert.setMessage("Escribe el enlace:");
+        alert.setView(editText);
+        alert.setPositiveButton("Añadir", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                String nuevoEnlace = editText.getText().toString();
+
+                if(!nuevoEnlace.startsWith("http://"))
+                    nuevoEnlace = "http://" + nuevoEnlace;
+
+                arrayEnlaces.add(nuevoEnlace);
+
+                ListView listaEnlaces = findViewById(R.id.listaEnlaces);
+
+                listaEnlaces.setAdapter(new ArrayAdapter<String>(Enlaces.this, android.R.layout.simple_list_item_1, arrayEnlaces));
+                Toast.makeText(Enlaces.this, "Enlace añadido correctamente.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alert.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                Toast.makeText(Enlaces.this, "No se ha añadido el enlace.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alert.show();
     }
 }
